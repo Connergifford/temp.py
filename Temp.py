3c67fd1087bf1c5aa546e5368f54a80ecefd40af
Connergifford <111781584+Connergifford@users.noreply.github.com>
2022-11-28 11:30:24 -0500

    Add files via upload

---
+++ Temp.py
@@ -0,0 +1,18 @@
+def temp(): 
+   
+    if temp is (90 or 'more'):
+        print
+    
+
+
+    input 
+    temp
+    
+if temp is  90:
+    print('the temp is too hot')
+
+if temp is 65:
+    print('too cold')
+
+if temp is 66 or 89:
+    print('just right')
